bf3368f96650ce0495ca1d570c5981b13c50984a
lukemayo42 <123115913+lukemayo42@users.noreply.github.com>
2025-02-16 15:02:37 -0500

    motors

--- analyze.py
+++ analyze.py
@@ -1,9 +1,13 @@
-import numpy
+import numpy as np
 import matplotlib.pyplot
 
-backLegSensorValues = numpy.load("data/backLegSensorValues.npy")
-frontLegSensorValues = numpy.load("data/frontLegSensorValues.npy")
-matplotlib.pyplot.plot(backLegSensorValues, linewidth=2)
-matplotlib.pyplot.plot(frontLegSensorValues)
+backLegSensorValues = np.load("data/backLegSensorValues.npy")
+frontLegSensorValues = np.load("data/frontLegSensorValues.npy")
+targetAngles = np.load("data/backLegTargetAngles.npy")
+frontLeg = np.load("data/frontLegTargetAngles.npy")
+#matplotlib.pyplot.plot(backLegSensorValues, linewidth=2)
+#matplotlib.pyplot.plot(frontLegSensorValues)
+matplotlib.pyplot.plot(targetAngles)
+matplotlib.pyplot.plot(frontLeg)
 matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
